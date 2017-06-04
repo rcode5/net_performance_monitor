a2d3d94c11655968710da5d2a6d0f80171a3c96d
Mr Rogers <jon@carbonfive.com>
2017-06-04 15:38:36 -0700

    Add more specs around writers

---
+++ spec/services/file_writer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require_relative '../spec_helper'
+require_relative '../../lib/services/file_writer'
+
+require 'fileutils'
+require 'tempfile'
+
+describe FileWriter do
+
+  let(:filename) { Tempfile.new.path }
+
+  subject(:service) { described_class.new(filename) }
+
+  describe '.new' do
+    it 'requires filename' do
+      expect{ described_class.new(nil) }.to raise_error FileWriter::InvalidFilename
+      expect{ described_class.new('') }.to raise_error FileWriter::InvalidFilename
+    end
+  end
+
+  describe '.write' do
+    it 'writes the contents to s3' do
+      service.write('the contents')
+      expect(File.open(filename).read).to eql 'the contents'
+      FileUtils.rm filename, force: true
+    end
+  end
+end
---
+++ spec/services/s3_writer_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require_relative '../spec_helper'
+require_relative '../../lib/services/s3_writer'
+
+describe S3Writer do
+
+  let(:bucket) { 'thebucket' }
+  let(:region) { 'theregion' }
+  let(:filename) { 'filename' }
+
+  subject(:service) { described_class.new(bucket, filename) }
+
+  before do
+    stub_const('ENV', {'AWS_REGION' => region})
+  end
+
+  describe '.new' do
+    it 'requires bucket and filename' do
+      expect{ described_class.new(nil, nil) }.to raise_error S3Writer::InvalidFilename
+      expect{ described_class.new(nil, 'file') }.to raise_error S3Writer::InvalidBucket
+      expect{ described_class.new('', '') }.to raise_error S3Writer::InvalidFilename
+      expect{ described_class.new('', 'file') }.to raise_error S3Writer::InvalidBucket
+    end
+  end
+
+  describe '.write' do
+    let(:mock_service) {
+      double(S3Service, put: 'success')
+    }
+    before do
+      allow(S3Service).to receive(:new).and_return(mock_service)
+    end
+    it 'writes the contents to s3' do
+      expect(service.write('the contents')).to eql 'success'
+      expect(S3Service).to have_received(:new).with( bucket: bucket, region: region )
+      expect(mock_service).to have_received(:put).with(filename, 'the contents')
+    end
+  end
+end
