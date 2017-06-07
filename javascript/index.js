b464ff7e1edc1034c775108a190c703d8bddd49d
Mr Rogers <jon@carbonfive.com>
2017-06-07 08:31:44 -0700

    Get react to render something

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+  render() {
+    debugger
+    return <div>hey</div>;
+  }
+}
+debugger
+ReactDOM.render( <App/>, document.getElementById('app'));
